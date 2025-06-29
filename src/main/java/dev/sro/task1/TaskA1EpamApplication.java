1e89547e52115db27cc88b49105613201c6208ff
sebastian <srestrep74@eafit.edu.co>
2025-06-29 12:43:07 -0500

    feat: create projectg

---
+++ src/main/java/dev/sro/task1/TaskA1EpamApplication.java
@@ -0,0 +1,13 @@
+package dev.sro.task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskA1EpamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskA1EpamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sro/task1/TaskA1EpamApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sro.task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskA1EpamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
